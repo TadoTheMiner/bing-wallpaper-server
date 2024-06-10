ead5bf406b694f41eea7ebecf5e2389a7c4c75d5
Tadeas Uradnik <taduradnik@gmail.com>
2024-06-10 18:46:50 +0200

    build(flake): fix the package

--- flake.nix
+++ flake.nix
@@ -1,55 +1,25 @@
 {
-  description = "A Nix-flake-based Rust development environment";
-
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
   };
-
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
-  }: let
-    overlays = [
-      rust-overlay.overlays.default
-      (final: prev: {
-        rustToolchain = let
-          rust = prev.rust-bin;
-        in
-          if builtins.pathExists ./rust-toolchain.toml
-          then rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain
-          then rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = ["rust-src" "rustfmt"];
-            };
-      })
-    ];
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit overlays system;};
-        });
-  in {
-    bing-wallpaper-server = nixpkgs.callPackage ./bing-wallpaper-server.nix;
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustToolchain
-          openssl
-          pkg-config
-          cargo-deny
-          cargo-edit
-          cargo-watch
-          rust-analyzer
-        ];
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.outputs.legacyPackages.${system};
+    in {
+      packages = {
+        bing-wallpaper-server = pkgs.callPackage ./bing-wallpaper-server.nix;
+
+        default = self.outputs.packages.${system}.bing-wallpaper-server;
       };
     });
-  };
 }
