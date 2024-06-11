e94db3ffda173ac61616f01a6b4e7301f8e41a0a
Tadeas Uradnik <taduradnik@gmail.com>
2024-06-11 11:11:17 +0200

    build: again

--- bing-wallpaper-server.nix
+++ bing-wallpaper-server.nix
@@ -3,11 +3,11 @@
   lib,
   ...
 }:
-pkgs.rustPlatform.buildRustPackage {
+pkgs.rustPlatform.buildRustPackage rec {
   name = "bing-wallpaper-server";
   src = lib.cleanSource "./";
   doCheck = false;
-  cargoLock.lockFile = "./Cargo.lock";
+  cargoLock.lockFile = "${src}/Cargo.lock";
   nativeBuildInputs = [pkgs.pkg-config];
   buildInputs = [pkgs.openssl];
 }
